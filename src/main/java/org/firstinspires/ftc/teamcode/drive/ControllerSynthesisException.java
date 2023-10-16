6323fdb5ed1436f55367738590f9763a5edd6b28
dpeng19 <148024848+dpeng19@users.noreply.github.com>
2023-10-15 22:47:50 -0400

    Delete src/main directory

--- src/main/java/org/firstinspires/ftc/teamcode/drive/ControllerSynthesisException.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.drive;
-
-public class ControllerSynthesisException extends Exception{
-    public ControllerSynthesisException() {}
-
-    public ControllerSynthesisException(String message) {
-        super(message);
-    }
-}
